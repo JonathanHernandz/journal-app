7cccc8a2fad53cc54c504efb9afc59c8ecaa1a71
Jonathan <electroc_jonathan@hotmail.com>
2024-03-23 06:34:48 -0600

    firebase config update

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -6,15 +6,6 @@ import { getFirestore } from 'firebase/firestore/lite'
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBjAjedPXl7BzLBoTvWU-rK_sGUmgZDOV4",
-//   authDomain: "react-cursos-e4771.firebaseapp.com",
-//   projectId: "react-cursos-e4771",
-//   storageBucket: "react-cursos-e4771.appspot.com",
-//   messagingSenderId: "456529093605",
-//   appId: "1:456529093605:web:222a8f910c06735538850c"
-// };
-
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APP_apiKey,
